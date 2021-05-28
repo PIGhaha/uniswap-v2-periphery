222b17a5d6e2994f9d386b9bb97bbc07a4aba483
PIGhaha <78994667+PIGhaha@users.noreply.github.com>
2021-05-28 19:28:32 +0800

    Update UniswapV2Router02.sol

--- contracts/UniswapV2Router02.sol
+++ contracts/UniswapV2Router02.sol
@@ -31,7 +31,8 @@ contract UniswapV2Router02 is IUniswapV2Router02 {
         factory = _factory;
         WETH = _WETH;
     }
-
+    
+    //只能从给WETH合约接受ETH
     receive() external payable {
         assert(msg.sender == WETH); // only accept ETH via fallback from the WETH contract
     }
@@ -45,15 +46,19 @@ contract UniswapV2Router02 is IUniswapV2Router02 {
         uint amountAMin,
         uint amountBMin
     ) internal virtual returns (uint amountA, uint amountB) {
-        // create the pair if it doesn't exist yet
+        // 如果交易对不存在则创建之
         if (IUniswapV2Factory(factory).getPair(tokenA, tokenB) == address(0)) {
             IUniswapV2Factory(factory).createPair(tokenA, tokenB);
         }
+        //获取两种代币的储备量
         (uint reserveA, uint reserveB) = UniswapV2Library.getReserves(factory, tokenA, tokenB);
+        
+        //如果交易对刚创建，则拟注入的代币全部转化为流动性
         if (reserveA == 0 && reserveB == 0) {
             (amountA, amountB) = (amountADesired, amountBDesired);
         } else {
             uint amountBOptimal = UniswapV2Library.quote(amountADesired, reserveA, reserveB);
+            //选择以哪种代币为标准计算实际注入数量，（以代币少的计算实际注入数量）
             if (amountBOptimal <= amountBDesired) {
                 require(amountBOptimal >= amountBMin, 'UniswapV2Router: INSUFFICIENT_B_AMOUNT');
                 (amountA, amountB) = (amountADesired, amountBOptimal);
@@ -65,6 +70,7 @@ contract UniswapV2Router02 is IUniswapV2Router02 {
             }
         }
     }
+    //用户调用的接口
     function addLiquidity(
         address tokenA,
         address tokenB,
@@ -75,12 +81,17 @@ contract UniswapV2Router02 is IUniswapV2Router02 {
         address to,
         uint deadline
     ) external virtual override ensure(deadline) returns (uint amountA, uint amountB, uint liquidity) {
+        //调用_addLiquidity函数计算向交易对合约注入的实际代币数量
         (amountA, amountB) = _addLiquidity(tokenA, tokenB, amountADesired, amountBDesired, amountAMin, amountBMin);
+        //获取交易对地址
         address pair = UniswapV2Library.pairFor(factory, tokenA, tokenB);
+        //将代币转移至交易对
         TransferHelper.safeTransferFrom(tokenA, msg.sender, pair, amountA);
         TransferHelper.safeTransferFrom(tokenB, msg.sender, pair, amountB);
+        //增发流动性
         liquidity = IUniswapV2Pair(pair).mint(to);
     }
+    //
     function addLiquidityETH(
         address token,
         uint amountTokenDesired,
